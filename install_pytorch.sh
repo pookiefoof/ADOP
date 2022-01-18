a24ab54595c6f57a3b3e821adc856a9a668738e6
Yanpei Cao <caoyanpei@gmail.com>
2022-01-18 17:08:37 +0800

    Fix CentOS build.

--- install_pytorch.sh
+++ install_pytorch.sh
@@ -4,27 +4,15 @@ git submodule update --init --recursive --jobs 0
 source $(conda info --base)/etc/profile.d/conda.sh
 conda activate adop
 
+gcc --version
+which gcc
 
 cd External/pytorch
 
-
-
-if command -v g++-9 &> /dev/null 
-then
-    export CC=$(which gcc-9)
-    export CXX=$(which g++-9)
-    export CUDAHOSTCXX=$(which g++-9)
-    echo "Using g++-9"
-elif command -v g++-7 &> /dev/null
-then
-    export CC=$(which gcc-7)
-    export CXX=$(which g++-7)
-    export CUDAHOSTCXX=$(which g++-7)
-    echo "Using g++-7"
-else
-    echo "No suitable compiler found. Install g++-7 or g++-9"
-    exit
-fi
+export CC=$(which gcc)
+export CXX=$(which g++)
+export CUDAHOSTCXX=$(which g++)
+echo "Using g++-7"
 
 unset CUDA_HOME
 
