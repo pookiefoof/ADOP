2513f40ead5ad77f95994a5e8596c6db54203475
Yanpei Cao <caoyanpei@gmail.com>
2022-01-24 11:25:41 +0800

    Update install_pytorch.sh

--- install_pytorch.sh
+++ install_pytorch.sh
@@ -20,12 +20,12 @@ export CMAKE_PREFIX_PATH=${CONDA_PREFIX:-"$(dirname $(which conda))/../"}
 export nproc=16
 python setup.py install
 
-cd /data
+# cd /data
 
-wget https://github.com/protocolbuffers/protobuf/releases/download/v3.13.0/protobuf-cpp-3.13.0.zip
-unzip protobuf-cpp-3.13.0.zip && cd protobuf-3.13.0
-./autogen.sh
-./configure --prefix=/data/miniconda3/envs/adop 
-make -j$(nproc)
-make check
-make install && ldconfig
+# wget https://github.com/protocolbuffers/protobuf/releases/download/v3.13.0/protobuf-cpp-3.13.0.zip
+# unzip protobuf-cpp-3.13.0.zip && cd protobuf-3.13.0
+# ./autogen.sh
+# ./configure --prefix=/data/miniconda3/envs/adop 
+# make -j$(nproc)
+# make check
+# make install && ldconfig
